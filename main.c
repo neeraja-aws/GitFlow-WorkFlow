fefc9241e584c24fbed5bd6d003ff7ef9d936356
Ubuntu <ubuntu@ip-172-31-11-180.us-east-2.compute.internal>
2021-10-22 14:10:48 +0000

    Extra functionality added in main.c for bug fixes

--- main.c
+++ main.c
@@ -3,10 +3,18 @@ Pseudo code
 Initial Function()
 {
 	//sample code
+	//Added some extra code in feature1 for bug fixes
 }
+
 Feature1()
 {
 	//This is feature1 code
 }
+
+Feature2()
+{
+	//This is feature2 code
+{
+
 
 }
